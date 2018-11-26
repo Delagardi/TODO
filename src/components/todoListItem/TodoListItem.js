d09acc4e1d507725b30109e649e100ef632fe3ae
Delagardi <delagardi@ymail.com>
2018-11-26 06:38:11 +0200

    Change dumb components to smart components (ItemStatusFilter and TodoListItem)

--- src/components/todoListItem/TodoListItem.js
+++ src/components/todoListItem/TodoListItem.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todoListItem.css';
 //TODO something
 
-const TodoListItem = ( { label, important = false }) => {
-  const styleItem = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  }
-  return (
-    <span className="todo-list-item">
-      <span 
-        className="todo-list-item-label"
-        style={styleItem}>
-        {label}
-      </span>
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-    </span>
-  );
-}
+export default class TodoListItem extends Component {
+  render() {
+    const { label, important = false } = this.props;
 
-export default TodoListItem;
+    const styleItem = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    }
+    return (
+      <span className="todo-list-item">
+        <span 
+          className="todo-list-item-label"
+          style={styleItem}>
+          {label}
+        </span>
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation" />
+        </button>
+      </span>
+    );
+  }
+}
